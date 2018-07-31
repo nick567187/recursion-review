93a551af7e3c7d1849c4adf6ae9f13877994085e
Hack Reactor Student <student@hackreactor.com>
2018-07-30 19:53:37 -0700

    in progress parseJSON

--- spec/parseJSONSpec.js
+++ spec/parseJSONSpec.js
@@ -3,12 +3,14 @@ describe('parseJSON', function() {
 
   it('should match the result of calling JSON.parse', function() {
     parseableStrings.forEach(function(test) {
+// debugger
       var result = parseJSON(test);
       var expected = JSON.parse(test);
       var equality = _.isEqual(result, expected); // why can't we use `===` here?
       // Replace this line with an `expect` statement that tests
       // the behavior described by the `it` string
-      throw new Error('Test is missing.')
+      expect(result).to.deep.equal(expected)
+      // throw new Error('Test is missing.')
     });
   });
 
--- src/parseJSON.js
+++ src/parseJSON.js
@@ -3,5 +3,51 @@
 
 // but you're not, so you'll write it from scratch:
 var parseJSON = function(json) {
-  // your code goes here
+	let curr = arguments[1] || 0;
+	let curKey = arguments[2] || '';
+	let curVal = arguments[3] || '';
+	let curChar = json[curr];
+	var curElt = arguments[4];
+
+	if(curChar === '[') {
+		if(json[1] === ']') {
+			return [];
+		}    
+	}
+
+	if(curChar === '{') {
+		// let obj = {};
+		curElt = arguments[4] || {};
+		
+
+		// curr++;
+		// if(json[1] === '}') {
+		// 	return obj;
+	} 
+
+	if(curChar === '"' && typeof curElt === 'object') {  
+		curr++;
+		if(curKey === '') {
+			while(json[curr] !== '"') {
+				curKey += json[curr];
+				curr++;
+			}
+		} else {
+			while(json[curr] !== '"') {
+				curVal += json[curr];
+				curr++;
+			}
+      curElt[curKey] = curVal;
+      curKey = '';
+      curVal = '';
+		}
+	}	
+
+	if(curChar == '}'){
+		return curElt;
+	}
+
+	curr++;
+	return parseJSON(json, curr, curKey, curVal, curElt); 
+  
 };
